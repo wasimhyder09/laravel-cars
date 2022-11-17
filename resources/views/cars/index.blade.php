0f73cb0d8e879b68ed9b9aecf78d7d0e41c3e773
Wasim Hyder <wasim@creativefaze.com>
2022-11-17 21:57:55 +0500

    Created tailwind css

--- resources/views/cars/index.blade.php
+++ resources/views/cars/index.blade.php
@@ -1,1 +1,9 @@
-cars
+@extends('layouts.app')
+
+@section('content')
+    <div class="m-auto w-4/5 py-24">
+      <div class="text-center">
+        <h1 class="text-5xl uppercase bold">Cars</h1>
+      </div>
+    </div>
+@endsection
