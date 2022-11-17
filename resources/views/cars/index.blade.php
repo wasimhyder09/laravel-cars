8dee56b918abb9cd62910227d5500a5455a9f6ec
Wasim Hyder <wasim@creativefaze.com>
2022-11-17 22:06:02 +0500

    Created cars display

--- resources/views/cars/index.blade.php
+++ resources/views/cars/index.blade.php
@@ -5,5 +5,19 @@
       <div class="text-center">
         <h1 class="text-5xl uppercase bold">Cars</h1>
       </div>
+      <div class="w-5/6 py-10">
+        <div class="m-auto">
+          <span class="uppercase text-blue-500 font-bold text-xs italic">
+            Founded: 2020
+          </span>
+          <h2 class="text-gray-700 text-5xl">
+            Audi
+          </h2>
+          <p class="text-lg text-gray-700 py-6">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut illum quis ipsam, quasi temporibus tempore. Fuga, alias saepe pariatur minima consectetur at sequi magnam, porro rerum quo officia similique a?
+          </p>
+          <hr class="mt-4 mb-8">
+        </div>
+      </div>
     </div>
 @endsection
